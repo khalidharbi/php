ed9b82d8de9008b85e056f5ad7de447c813f30fe
khalid harbi <ka901od@gmail.com>
2023-10-30 23:48:15 +0300

    add var

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
+<?php 
+//Variables 
+$title = "learn php "
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo "learn php "?></title>
+    <title><?php echo $title ?></title>
 </head>
 <body>
       <?php echo "<h1>Welcome to my Website </h1>" ?>
