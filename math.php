3159d2d5fe52c9ac0b22e88aa6df570607800806
khalid harbi <ka901od@gmail.com>
2023-11-06 03:09:38 +0300

    add line

--- math.php
+++ math.php
@@ -1,5 +1,18 @@
 <?php
 
-echo 20 * 10;
+//echo 20 * 10;
+//echo pow(5, 3);
+//echo sqrt(144);
+//echo round(3.6);
+//echo floor(3.8);
+//echo ceil(3.8);
+//echo 10 % 3;
 
- ?>
+//$num =5 ;
+//$num+=5;
+//echo $num;
+
+//echo max(3, 10);
+//echo min(3, 10);
+
+?>
