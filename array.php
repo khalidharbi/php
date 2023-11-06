edf182a74396e7fac08c8d84c219a246f63d7fcd
khalid harbi <ka901od@gmail.com>
2023-11-07 01:13:54 +0300

    commit

--- array.php
+++ array.php
@@ -1,4 +1,12 @@
+ <form action="array.php" method="GET">
+    <input type="text" name="xname">
+    <input type="submit">
+ </form>
+ 
+ 
  <?php 
+$xname = $_GET["xname"];
+
 
 //$family = array("Father", "Mother", "Brother");
 
@@ -9,6 +17,7 @@ $family = [
     "designer" => "majed",
      "gamer" =>"anas"
         ];
-echo $family["gamer"]
+        
+echo $family[$xname]
 
 ?>
